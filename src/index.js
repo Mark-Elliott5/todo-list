8e8d72d2efcf01b8d53030a019cd299c25552ec9
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-04-23 23:31:36 -0400

    Update gitignore

--- src/index.js
+++ src/index.js
@@ -1,18 +1,47 @@
+import domManipulator from "./domManipulator.js";
+
 const todo = (title, description, dueDate, priority) => {
-  const updateTitle = () => {};
-  const updateDesc = () => {};
-  const updateDueDate = () => {};
-  const updatePriority = () => {};
-  const obj = { title, description, dueDate, priority };
+  const setTitle = (input) => {
+    obj.title = input;
+  };
+  const setDesc = () => {};
+  const setDueDate = () => {};
+  const setPriority = () => {};
+  const getTitle = () => obj.title;
+  const getDesc = () => {};
+  const getDueDate = () => {};
+  const getPriority = () => {};
+  const obj = {
+    title,
+    description,
+    dueDate,
+    priority,
+    setTitle,
+    setDesc,
+    setDueDate,
+    setPriority,
+    getTitle,
+    getDesc,
+    getDueDate,
+    getPriority,
+  };
   return obj;
 };
 
-const project = () => {
+const project = (title, description) => {
   const todoList = [];
   const addToProject = (todo) => {
     todoList.push(todo);
-    domManipulator.update();
+    domManipulator.set();
   };
   const obj = { todoList, addToProject };
   return obj;
+};
+
+const appLogic = () => {
+  const createProject = (title, description) => {
+    const newProject = new project(title, description);
+  };
+  const markComplete = () => {};
+  const changePriority = () => {};
 };
