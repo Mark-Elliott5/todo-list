80f1a07766b72526675d06fa3b472e189b01e670
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-06-16 09:28:00 -0400

    Functions, modules, styles

--- src/domManipulator.js
+++ src/domManipulator.js
@@ -1,3 +1,83 @@
-const domManipulator = () => {
-  const content = document.getElementById("content");
-};
+import Icon from './images/github-mark-white.png';
+
+const domManipulator = (() => {
+  const pageLoad = () => {
+    // sidebar
+    const content = document.getElementById('content');
+    const sidebar = Object.assign(document.createElement('div'), {
+      id: 'sidebar',
+    });
+    const searchbarWrapper = Object.assign(document.createElement('div'), {
+      id: 'searchbarwrapper',
+    });
+    const searchHeading = Object.assign(document.createElement('h1'), {
+      textContent: 'Search',
+    });
+    searchbarWrapper.appendChild(searchHeading);
+    const projectBtnWrapper = Object.assign(document.createElement('div'), {
+      id: 'projectbtnwrapper',
+    });
+    const createProjectButton = Object.assign(
+      document.createElement('button'),
+      {
+        id: 'createprojectbutton',
+        innerText: '+',
+      }
+    );
+    const deleteProjectButton = Object.assign(
+      document.createElement('button'),
+      {
+        id: 'deleteprojectbutton',
+        innerText: '-',
+      }
+    );
+    projectBtnWrapper.append(createProjectButton, deleteProjectButton);
+    const searchbarForm = Object.assign(document.createElement('form'), {
+      id: 'searchbarform',
+    });
+    const searchbar = Object.assign(document.createElement('input'), {
+      id: 'searchbar',
+      placeholder: 'Search Projects',
+      ariaLabel: 'Search Query',
+    });
+    searchbarForm.appendChild(searchbar);
+    searchbarWrapper.append(searchbarForm, projectBtnWrapper);
+    const listOfProjects = Object.assign(document.createElement('div'), {
+      id: 'projectlist',
+    });
+    const projectListHeading = Object.assign(document.createElement('h1'), {
+      textContent: 'Projects',
+    });
+    listOfProjects.appendChild(projectListHeading);
+    sidebar.append(searchbarWrapper, listOfProjects);
+
+    // project pane
+    const projectPane = Object.assign(document.createElement('div'), {
+      id: 'projectpane',
+    });
+
+    // main content wrapper
+
+    const mainContentWrapper = Object.assign(document.createElement('div'), {
+      id: 'maincontentwrapper',
+    });
+    mainContentWrapper.append(sidebar, projectPane);
+
+    const footer = document.createElement('footer');
+    const githubLink = Object.assign(document.createElement('a'), {
+      innerText: 'Designed by Mark Elliott',
+      href: 'https://github.com/Mark-Elliott5',
+    });
+    const githubIcon = new Image();
+    githubIcon.src = Icon;
+    githubIcon.classList.add('github-link');
+    githubLink.appendChild(githubIcon);
+    footer.appendChild(githubLink);
+
+    content.append(mainContentWrapper, footer);
+  };
+
+  return { pageLoad };
+})();
+
+export default domManipulator;
--- src/index.js
+++ src/index.js
@@ -1,47 +1,16 @@
-import { doc } from 'prettier';
-import domManipulator from './domManipulator.js';
-import Icon from './images/github-mark-white.png';
-import './styles.css';
+import domManipulator from './domManipulator';
+// import './styles.css';
 
-// sidebar
-const content = document.getElementById('content');
-const sidebar = document.createElement('div');
-const searchbarWrapper = Object.assign(document.createElement('div'), {
-  id: 'searchbarwrapper',
-});
-const projectBtnWrapper = Object.assign(document.createElement('div'), {
-  id: 'projectbtnwrapper',
-});
-const createProjectButton = Object.assign(document.createElement('button'), {
-  id: 'createprojectbutton',
-  innerText: '+',
-});
-const deleteProjectButton = Object.assign(document.createElement('button'), {
-  id: 'deleteprojectbutton',
-  innerText: '-',
-});
-projectBtnWrapper.append(createProjectButton, deleteProjectButton);
-const searchbar = Object.assign(document.createElement('form'), {
-  id: 'searchbar',
-  placeholder: 'Search Projects',
-});
-searchbarWrapper.append(searchbar, projectBtnWrapper);
-const listOfProjects = Object.assign(document.createElement('div'), {
-  id: 'projectlist',
-});
-sidebar.append(searchbarWrapper, listOfProjects);
-content.appendChild(sidebar);
+// const appLogic = () => {
+//   let index = 0;
+//   const projectArray = [];
+//   const createProject = (title, description) => {
+//     const newProject = new Project(title, description, (index += 1));
+//     projectArray.push(newProject);
+//     domManipulator.update();
+//   };
+//   const markComplete = () => {};
+//   const changePriority = () => {};
+// };
 
-// project view box
-
-const appLogic = () => {
-  const index = 0;
-  const projectArray = [];
-  const createProject = (title, description) => {
-    const newProject = new Project(title, description, ++index);
-    projectArray.push(newProject);
-    domManipulator.update();
-  };
-  const markComplete = () => {};
-  const changePriority = () => {};
-};
+document.addEventListener('DOMContentLoaded', domManipulator.pageLoad);
