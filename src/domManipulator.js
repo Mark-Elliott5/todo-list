ced08e9880af6de540f9728d9410a796f16c620f
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-06-20 20:30:35 -0400

    Created createNewProject function

--- src/domManipulator.js
+++ src/domManipulator.js
@@ -1,6 +1,179 @@
 import Icon from './images/github-mark-white.png';
 
 const domManipulator = (() => {
+  const createNewProject = () => {
+    console.log('create project');
+    const createProjectBlocker = Object.assign(document.createElement('div'), {
+      id: 'createprojectblocker',
+      classList: 'blocker',
+    });
+
+    const createProjectBox = Object.assign(document.createElement('div'), {
+      id: 'createprojectbox',
+    });
+
+    const createProjectHeading = Object.assign(document.createElement('h1'), {
+      textContent: 'Create a New Project',
+    });
+
+    const createProjectForm = Object.assign(document.createElement('form'), {
+      id: 'createprojectform',
+    });
+
+    const nameFieldWrapper = document.createElement('div');
+    const nameFieldLabel = Object.assign(document.createElement('label'), {
+      for: 'projectname',
+      textContent: 'Project Name',
+    });
+    const nameFieldInput = Object.assign(document.createElement('input'), {
+      id: 'projectname',
+      name: 'name',
+      required: true,
+      maxLength: '40',
+      minLength: '5',
+      type: 'text',
+    });
+    nameFieldWrapper.append(nameFieldLabel, nameFieldInput);
+
+    const descriptionFieldWrapper = document.createElement('div');
+    const descriptionFieldLabel = Object.assign(
+      document.createElement('label'),
+      {
+        for: 'projectdescription',
+        textContent: 'Description',
+      }
+    );
+    const descriptionFieldInput = Object.assign(
+      document.createElement('input'),
+      {
+        id: 'projectdescription',
+        name: 'description',
+        required: true,
+        maxLength: '40',
+        type: 'text',
+      }
+    );
+    descriptionFieldWrapper.append(
+      descriptionFieldLabel,
+      descriptionFieldInput
+    );
+
+    const duedateFieldWrapper = document.createElement('div');
+    const duedateFieldLabel = Object.assign(document.createElement('label'), {
+      for: 'projectduedate',
+      textContent: 'Due Date',
+    });
+    const duedateFieldInput = Object.assign(document.createElement('input'), {
+      id: 'projectduedate',
+      name: 'duedate',
+      required: true,
+      maxLength: '40',
+      type: 'date',
+    });
+    duedateFieldWrapper.append(duedateFieldLabel, duedateFieldInput);
+
+    const priorityFieldWrapper = document.createElement('div');
+    const priorityFieldHeading = Object.assign(document.createElement('h1'), {
+      textContent: 'Priority',
+    });
+    const priorityFieldLabelLow = Object.assign(
+      document.createElement('label'),
+      {
+        for: 'projectprioritylow',
+        textContent: 'Low',
+      }
+    );
+    const priorityFieldInputLow = Object.assign(
+      document.createElement('input'),
+      {
+        id: 'projectprioritylow',
+        name: 'priority',
+        required: true,
+        type: 'radio',
+        textContent: 'Low',
+        value: 'Low',
+      }
+    );
+    const priorityFieldLabelNormal = Object.assign(
+      document.createElement('label'),
+      {
+        for: 'projectprioritynormal',
+        textContent: 'Normal',
+      }
+    );
+    const priorityFieldInputNormal = Object.assign(
+      document.createElement('input'),
+      {
+        id: 'projectprioritynormal',
+        name: 'priority',
+        required: true,
+        type: 'radio',
+        textContent: 'Normal',
+        value: 'Normal',
+      }
+    );
+    const priorityFieldLabelHigh = Object.assign(
+      document.createElement('label'),
+      {
+        for: 'projectpriorityhigh',
+        textContent: 'High',
+      }
+    );
+    const priorityFieldInputHigh = Object.assign(
+      document.createElement('input'),
+      {
+        id: 'projectpriorityhigh',
+        name: 'priority',
+        required: true,
+        type: 'radio',
+        textContent: 'High',
+        value: 'High',
+      }
+    );
+    priorityFieldWrapper.append(
+      priorityFieldHeading,
+      priorityFieldLabelLow,
+      priorityFieldInputLow,
+      priorityFieldLabelNormal,
+      priorityFieldInputNormal,
+      priorityFieldLabelHigh,
+      priorityFieldInputHigh
+    );
+
+    const submitNewProjectWrapper = document.createElement('div');
+    const submitNewProjectButton = Object.assign(
+      document.createElement('button'),
+      {
+        type: 'submit',
+        id: 'submitnewproject',
+        classList: 'submitbutton',
+        textContent: 'Create',
+      }
+    );
+    submitNewProjectWrapper.append(submitNewProjectButton);
+
+    createProjectForm.append(
+      nameFieldWrapper,
+      descriptionFieldWrapper,
+      duedateFieldWrapper,
+      priorityFieldWrapper,
+      submitNewProjectWrapper
+    );
+
+    createProjectBox.append(createProjectHeading, createProjectForm);
+    createProjectBlocker.append(createProjectBox);
+    createProjectBox.addEventListener('click', (e) => {
+      e.stopPropagation();
+    });
+    createProjectBlocker.addEventListener('click', () => {
+      const content = document.getElementById('content');
+      const child = document.getElementById('createprojectblocker');
+      content.removeChild(child);
+    });
+    const content = document.getElementById('content');
+    content.append(createProjectBlocker);
+  };
+
   const pageLoad = () => {
     // sidebar
     const content = document.getElementById('content');
@@ -10,7 +183,7 @@ const domManipulator = (() => {
     const searchbarWrapper = Object.assign(document.createElement('div'), {
       id: 'searchbarwrapper',
     });
-    const searchHeading = Object.assign(document.createElement('h1'), {
+    const searchHeading = Object.assign(document.createElement('h2'), {
       textContent: 'Search',
     });
     searchbarWrapper.appendChild(searchHeading);
@@ -24,6 +197,7 @@ const domManipulator = (() => {
         innerText: '+',
       }
     );
+    createProjectButton.addEventListener('click', createNewProject);
     const deleteProjectButton = Object.assign(
       document.createElement('button'),
       {
@@ -45,7 +219,7 @@ const domManipulator = (() => {
     const listOfProjects = Object.assign(document.createElement('div'), {
       id: 'projectlist',
     });
-    const projectListHeading = Object.assign(document.createElement('h1'), {
+    const projectListHeading = Object.assign(document.createElement('h2'), {
       textContent: 'Projects',
     });
     listOfProjects.appendChild(projectListHeading);
@@ -57,24 +231,29 @@ const domManipulator = (() => {
     });
 
     // main content wrapper
-
     const mainContentWrapper = Object.assign(document.createElement('div'), {
       id: 'maincontentwrapper',
     });
     mainContentWrapper.append(sidebar, projectPane);
+
+    // header and footer
+    const header = Object.assign(document.createElement('header'), {
+      textContent: 'Todo List',
+    });
 
     const footer = document.createElement('footer');
     const githubLink = Object.assign(document.createElement('a'), {
       innerText: 'Designed by Mark Elliott',
       href: 'https://github.com/Mark-Elliott5',
     });
-    const githubIcon = new Image();
-    githubIcon.src = Icon;
-    githubIcon.classList.add('github-link');
+    const githubIcon = Object.assign(new Image(), {
+      src: Icon,
+      classList: 'github-link',
+    });
     githubLink.appendChild(githubIcon);
     footer.appendChild(githubLink);
 
-    content.append(mainContentWrapper, footer);
+    content.append(header, mainContentWrapper, footer);
   };
 
   return { pageLoad };
