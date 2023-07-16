163df7b57d874dcb96d44a5236e204067ace7cf4
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-07-16 16:11:51 -0400

    Removed comments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
 import domManipulator from './domManipulator';
-// import './styles.css';
-
-// const appLogic = () => {
-//   let index = 0;
-//   const projectArray = [];
-//   const createProject = (title, description) => {
-//     const newProject = new Project(title, description, (index += 1));
-//     projectArray.push(newProject);
-//     domManipulator.update();
-//   };
-//   const markComplete = () => {};
-//   const changePriority = () => {};
-// };
 
 document.addEventListener('DOMContentLoaded', domManipulator.pageLoad);
